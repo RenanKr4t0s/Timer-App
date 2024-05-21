5ce610c1b0268b8f0f1d11aa5d9c79ff0c96c03a
RenanKr4t0s <redrums88@gmail.com>
2024-05-20 22:47:31 -0300

    Correções de funções

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 
 function App() {
+  let now = new Date().toLocaleTimeString();
+  const [time, setTime]=useState(now);
+
+  function updateTime(){
+    let newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  setInterval(updateTime,1000)
+
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
+    <div className="container">
+      <h2>{time}</h2>
+      <button onClick={updateTime}>Atualizar</button>
     </div>
   );
 }
